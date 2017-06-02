9060bca2a32699822acd0f4212bc5bafcd2d7ab3
Piyush <piyushjain1sa@gmail.com>
2017-06-03 00:18:54 +0530

    comparision script improved, graph included

--- devel/MyPolyUnion/comp.m
+++ devel/MyPolyUnion/comp.m
@@ -16,45 +16,43 @@
 
 ## Benchmarking polynomial union
 # We compare the oct-file implementation of polygon union of the clipper library
-# with the oine provided by th emex interface.
+# with the one provided by the mex interface.
 pkg load geometry
+
 c1 = [ 0.5, 0, 5];
 c2 = [-0.5, 0, 5];
-N  = 4;
+
+N = 5;
 p1 = circleAsPolygon (c1, N);
 p2 = circleAsPolygon (c2, N);
+PU = union(p1, p2, 3);
+cPU = clipPolygon(p1, p2, 3);
+
+# generating data for the elapsed time for both the methods over a parameter N
+for N=100:100:10000
+	p1 = circleAsPolygon (c1, N);
+	p2 = circleAsPolygon (c2, N);
+	tic
+	PU = union(p1, p2, 3);
+	elapsed_time_PU(N) = toc;
+	tic
+	cPU = clipPolygon(p1, p2, 3);
+	elapsed_time_cPU(N) = toc;
+end
+
+## Plotting graphs to compare the elapsed time of both
+x = 100:100:10000;
+y1 = elapsed_time_PU(x);
+y2 = elapsed_time_cPU(x);
+## polyUnion in yellow color
+plot(x,y1,'y');
+hold
+## clipPolygon in red color
+plot(x,y2,'r');
 
 
 
-tic
-union(p1, p2, 3);
-toc
-
-#tic
-#cPU = clipPolygon(p1, p2, 3);
-#toc
-
-#clf
-#drawPolygon (PU);
-#drawPolygon (cPU);
-
-# Piyush: Both are same problems, there was an issue in handling NaN as
-# delimeter, so I have provisionally used value-100000 in place of NaN
-#x1 = [0 0; 10 0; 10 10; 0 10; 0 0; NaN NaN; 2 2; 8 2; 8 8; 8 2; 2 2;];
-#y1 = [5 0; 15 0; 15 10; 5 10; 5 0; NaN NaN; 7 2; 7 8; 12 8; 12 2; 7 2;];
-
-#x2 = [0 0; 10 0; 10 10; 0 10; 0 0; -100000 -100000; 2 2; 8 2; 8 8; 8 2; 2 2;];
-#y2 = [5 0; 15 0; 15 10; 5 10; 5 0; -100000 -100000; 7 2; 7 8; 12 8; 12 2; 7 2;];
 
 
-#tic()
 
-#polyUnion(x2, y2)
 
-#toc()
-
-#tic()
-
-#clipPolygon(x1, y1, 3)
-
-#toc()
