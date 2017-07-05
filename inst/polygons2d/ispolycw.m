9077bef42b02f360492d864b8f4ae1d4f1a88301
Piyush <piyushjain1sa@gmail.com>
2017-07-05 22:23:31 +0530

    minor change in ispolycw

--- inst/polygons2d/ispolycw.m
+++ inst/polygons2d/ispolycw.m
@@ -1,5 +1,5 @@
 ## Copyright (C) 2016 - Juan Pablo Carbajal
-## Copyright (C) 2016 - Piyush Jain
+## Copyright (C) 2017 - Piyush Jain
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
